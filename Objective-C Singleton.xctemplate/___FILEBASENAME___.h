c13e62dde73c19e37de2592d0495acb573b7da0d
Filip Beć <filip.bec@gmail.com>
2013-04-01 11:31:34 +0200

    Added Singleton template

---
+++ Objective-C Singleton.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject
+
++ (___FILEBASENAMEASIDENTIFIER___*)sharedInstance;
++ (___FILEBASENAMEASIDENTIFIER___*)instance;
+
+@end
---
+++ Objective-C Singleton.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        //Add your custom initialization code here
+    }
+    return self;
+}
+
+#pragma mark - Singleton Methods
+
++ (___FILEBASENAMEASIDENTIFIER___*)sharedInstance {
+
+	static ___FILEBASENAMEASIDENTIFIER___ *_instance;
+	if(!_instance) {
+		static dispatch_once_t onceToken;
+		dispatch_once(&onceToken, ^{
+			_instance = [[self alloc] init];
+        });
+    }
+    return _instance;
+}
+
++ (___FILEBASENAMEASIDENTIFIER___*)instance {
+    return [self sharedInstance];
+}
+
+#pragma mark - Custom Methods
+
+// Add your custom methods here
+
+@end
